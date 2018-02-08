bf189f3872c8e55020d10c6fc3eab1935704bd3e
cooliotonyio <tonyzhao@berkeley.edu>
2018-02-08 13:41:27 -0800

    clean app.py, home.html

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-# We upgrade from a simple string to an HTML file
+# Getting a Flask server up and running
 
-from flask import Flask, render_template
+from flask import Flask
 import os
 
 app = Flask(__name__)
-
-@app.route('/')
-@app.route('/<name>')
-def home_page(name=None):
-	if not name:
-		name = "World"
-	return render_template("home.html", name = name)
 
 port = os.getenv('PORT', '8080')
 if __name__ == "__main__":
